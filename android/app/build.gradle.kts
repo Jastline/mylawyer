b40477d9d8bb0ff9bc56dc07d64ad6b6961cc73a
Jastline <dimanovikovlev@yandex.ru>
2025-04-21 03:48:24 +0300

    Хз, вроде рабочее
    flutter clean
    cd android
    ./gradlew clean
    cd ..
    flutter pub get
    flutter run

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,16 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.JastLine.mylawyer"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
+    //compileSdk = flutter.compileSdkVersion
+    //ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -20,21 +22,27 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.JastLine.mylawyer"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
+        versionCode = flutter.versionCode.toInt()
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        //release {
+            //signingConfig = signingConfigs.getByName("debug")
+        //}
+        debug {
+            isMinifyEnabled = false
         }
     }
 }
